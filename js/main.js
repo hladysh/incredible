ef6f82ff0ef3bb59f245f82d5916b57a3d714cde
Ivan <hladysh.im@gmail.com>
2016-12-14 11:54:45 +0000

    add media

---
+++ js/main.js
@@ -0,0 +1,9 @@
+		$(document).on('ready', function() {
+		    $(".regular").slick({
+		        dots: false,
+		        infinite: true,
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		    });
+		});
+		
